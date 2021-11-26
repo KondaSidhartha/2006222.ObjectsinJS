926b209d57179e525420911d337a71af0b722e1f
KondaSidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2021-11-26 21:39:47 +0530

    Add files via upload

---
+++ js4.js
@@ -0,0 +1,24 @@
+var person=new Object();
+person:{
+    var firstname;
+    var lastname;
+    var officeaddress;
+}
+person.name1=new Object();
+person.name1.firstname="KONDA";
+person.name1.lastname="SIDHARTHA";
+person.name1.officeaddress="WANAPARTHY";
+console.log("1st person details");
+console.log("first name:"+person.name1.firstname+"\nlast name:"+person.name1.lastname+"\nofficeaddress:"+person.name1.officeaddress);
+person.name2=new Object();
+person.name2.firstname="SREEVALLI";
+person.name2.lastname="BAKSHI";
+person.name2.officeaddress="WANAPARTHY";
+console.log("2nd person details");
+console.log("first name:"+person.name2.firstname+"\nlast name:"+person.name2.lastname+"\nofficeaddress:"+person.name2.officeaddress);
+person.name3=new Object();
+person.name3.firstname="SAHITHI";
+person.name3.lastname="MANOGNA";
+person.name3.officeaddress="HYDERABAD";
+console.log("3rd person details");
+console.log("first name:"+person.name3.firstname+"\nlast name:"+person.name3.lastname+"\nofficeaddress:"+person.name3.officeaddress);
